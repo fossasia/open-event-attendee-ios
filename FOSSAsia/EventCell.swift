9232a1b6a1f3546f09b3a6d3ee82ed9a22bb74b5
Jurvis Tan <jurvis.tan@me.com>
2016-02-01 10:34:24 +0800

    remove Reveal.framework

--- FOSSAsia/EventCell.swift
+++ FOSSAsia/EventCell.swift
@@ -14,12 +14,10 @@ class EventCell: UITableViewCell {
     @IBOutlet weak var typeView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timingLabel: UILabel!
-
     private var delegate: EventCellWithTypePresentable?
     
     func configure(withPresenter presenter: EventCellWithTypePresentable) {
         delegate = presenter
-        
         titleLabel.text = delegate!.eventName
         typeView.backgroundColor = delegate!.typeColor
         timingLabel.text = delegate!.timing
