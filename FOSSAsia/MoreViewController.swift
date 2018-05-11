7abaa77a58693b69471a8671ca74ef7da33c6ba9
baquer <baquer@github.com>
2018-05-12 03:00:59 +0530

    Update and add more functions.

--- FOSSAsia/MoreViewController.swift
+++ FOSSAsia/MoreViewController.swift
@@ -15,6 +15,7 @@ private struct DefaultURLs {
     static let fossaisaTwitter = "https://twitter.com/fossasia"
     static let fossasiaItune = "https://itunes.apple.com/us/app/fossasia/id1089164461?ls=1&mt=8"
     static let fossasiaGoogleGroups = "http://groups.google.com/group/fossasia"
+    static let fossasiaFacebook = "https://www.facebook.com/fossasia/share"
 }
 
 class MoreViewController: UITableViewController, MFMailComposeViewControllerDelegate {
@@ -107,14 +108,41 @@ class MoreViewController: UITableViewController, MFMailComposeViewControllerDele
             self.present(self.createSVC(DefaultURLs.fossasiaGoogleGroups), animated: true, completion: nil)
             break
         case 4:
-            let mailVc = configureMailComposeViewController(recipient: "",
-                                                            subject: "Check out the Open Event iOS!",
-                                                            message: "I use the Open Event iOS for browsing information about the event visit https://fossasia.org/ for more info")
-            if MFMailComposeViewController.canSendMail() {
-                self.present(mailVc, animated: true, completion: nil)
-            } else {
-                self.showSendMailErrorAlert()
+            let myActionSheet = UIAlertController(title: "Tell A Friend", message: "Whom you want to tell?", preferredStyle: UIAlertControllerStyle.actionSheet)
+            
+            // email action button
+            let emailAction = UIAlertAction(title: "Email", style: UIAlertActionStyle.default) { (action) in
+                let mailVc = self.configureMailComposeViewController(recipient: "", subject: "Check out the Open Event iOS!",
+                                                                           message: "I use the Open Event iOS for browsing information about the event visit https://fossasia.org/ for more info")
+                            if MFMailComposeViewController.canSendMail() {
+                                self.present(mailVc, animated: true, completion: nil)
+                            } else {
+                                self.showSendMailErrorAlert()
+                            }
             }
+            
+            // facebook action button
+            let facebookAction = UIAlertAction(title: "Facebook", style: UIAlertActionStyle.default) { (action) in
+                self.present(self.createSVC(DefaultURLs.fossasiaFacebook), animated: true, completion: nil)
+            }
+            
+            // Message action button
+            let messageAction = UIAlertAction(title: "Message", style: UIAlertActionStyle.default) { (action) in
+                self.sendMessageToNumber(number: "", message: "")
+            }
+            
+            // cancel action button
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (action) in
+            }
+            
+            // add action buttons to action sheet
+            myActionSheet.addAction(emailAction)
+            myActionSheet.addAction(facebookAction)
+            myActionSheet.addAction(messageAction)
+            myActionSheet.addAction(cancelAction)
+            
+            // present the action sheet
+            self.present(myActionSheet, animated: true, completion: nil)
         break
         default:
             break
@@ -123,5 +151,17 @@ class MoreViewController: UITableViewController, MFMailComposeViewControllerDele
 
     func createSVC(_ urlString: String) -> SFSafariViewController {
         return SFSafariViewController(url: URL(string: urlString)!)
+    }
+    func sendMessageToNumber(number:String,message:String){
+        
+        let sms = "sms:\(number)&body=\(message)"
+        let url = URL(string:sms)!
+        let shared = UIApplication.shared
+        
+        if(shared.canOpenURL(url)){
+            shared.open(url, options: [:], completionHandler: nil)
+        }else{
+            print("unable to send message")
+        }
     }
 }
