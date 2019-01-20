b1093a7a7ea09a375c529efb6d8767b7b8d9efb3
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2019-01-20 14:32:41 +0530

    Converting Fossasia Module into Swift4.2

--- FOSSAsia/AppDelegate.swift
+++ FOSSAsia/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UITabBar.appearance().tintColor = Colors.mainRedColor
         UINavigationBar.appearance().tintColor = Colors.creamTintColor
